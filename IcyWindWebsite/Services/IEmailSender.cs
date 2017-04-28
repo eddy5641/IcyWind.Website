5af226ae951a45f8595095ff2ba1030233b8a994
eddy5641 <eddydavinchi@gmail.com>
2017-04-27 21:48:48 -0700

    Add project files.

---
+++ IcyWindWebsite/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IcyWindWebsite.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
