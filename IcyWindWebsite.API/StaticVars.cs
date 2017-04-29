3daf718380d0abb56cb2f2145aaabd435920681e
eddy5641 <eddydavinchi@gmail.com>
2017-04-28 22:43:24 -0700

    Some updates

---
+++ IcyWindWebsite.API/StaticVars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IcyWindWebsite.API
+{
+    public static class StaticVars
+    {
+        /// <summary>
+        /// The connection string of the server
+        /// </summary>
+        //Default server=localhost;userid=user12;password=34klq*;database=mydb
+        public static string connString = @"server=35.184.49.175;userid=user;password=jZNYcm7ZPfU3zbDh28y3kfgjd2gUR5;" +
+            "database=icywinddb;SslMode=Required;";
+        //qlsFO0dCmm4P1u0a
+
+        /// <summary>
+        /// This key must be changed if you are running this on your own server for security reasons
+        /// </summary>
+        //Default M46IrenJ86WOI94j8968HE94ngji9
+        public const string AdminKey = @"jZNYcm7ZPfU3zbDh28y3kfgjd2gUR5";
+    }
+}
